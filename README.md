84fdc8edc119e28cfb0d6c1b20d7e4637927e72a
Hugo Holmqvist <hugo.holm@live.com>
2018-04-04 00:41:53 +0300

    Intial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Diodibot
+
+Discord bot for listening to [radiodiodi](https://radiodiodi.fi/).
+
+### Setting up the bot for development
+- Install [typescript](https://www.typescriptlang.org/)
+- run `yarn --production=false`
+- Define a environment variable `DIODIBOT_TOKEN` which is your discord bot token.
+- Run `tsc` to compile
+- Run `yarn start`
---
+++ src/App.ts
@@ -0,0 +1,49 @@
+require('dotenv').config();
+import { Client, Message, VoiceConnection } from 'eris';
+import * as axios from 'axios';
+import { ReadStream } from 'fs';
+
+const client = new Client(String(process.env.DIODIBOT_TOKEN));
+const STREAM_URL = 'https://virta.radiodiodi.fi/stream.mp3';
+
+async function run() {
+  // Get the radio stream
+  const radioStream = await axios.default({
+    method: 'get',
+    url: STREAM_URL,
+    responseType: 'stream'
+  });
+
+  // Listen for messageCreate events from discord
+  client.on('messageCreate', async (msg: Message) => {
+    // Check if the message start with !diodi join
+    if (msg.content.startsWith('!diodi join')) {
+      // Check if the callee is a server member
+      if (msg.member) {
+        let member = msg.member;
+        // Check if the user is in a voice channel
+        if (member.voiceState && member.voiceState.channelID) {
+          // Join the voice channel where the user is
+          client.joinVoiceChannel(member.voiceState.channelID).then((connection: VoiceConnection) => {
+            // Playback the stream
+            connection.play(radioStream.data);
+            client.createMessage(msg.channel.id, '***16.4.2018***');
+          }).catch(e => {
+            console.log(e);
+          });
+        }
+      }
+    }
+  });
+
+  // Set the status
+  client.editStatus('online', {
+    name: 'use !diodi join',
+    url: 'https://radiodiodi.fi/',
+    type: 2
+  });
+
+  client.connect();
+}
+
+run();
